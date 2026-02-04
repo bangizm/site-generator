3053ef78a8cf9a2f202cf832e7dda7385bc79f50
bangizm <bangizm@gmail.com>
2026-02-03 22:36:04 -0500

    Add split_nodes_delimiter function

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,5 +1,5 @@
 import unittest
-from textnode import TextNode, TextType
+from textnode import TextNode, TextType, split_nodes_delimiter
 
 class TestTextNode(unittest.TestCase):
     def test_eq(self):
@@ -45,7 +45,28 @@ class TestTextNode(unittest.TestCase):
         node2 = TextNode("This is a text node", TextType.IMAGE)
         self.assertNotEqual(node, node2)
     
+class TestSplitNodesDelimiter(unittest.TestCase):
+    def test_split_nodes_delimiter_basic(self):
+        nodes = [TextNode("This is **bold** text", TextType.TEXT)]
+        result = split_nodes_delimiter(nodes, "**", TextType.BOLD)
+        expected = [
+            TextNode("This is ", TextType.TEXT),
+            TextNode("bold", TextType.BOLD),
+            TextNode(" text", TextType.TEXT)
+        ]
+        self.assertEqual(result, expected)
 
+    def test_split_nodes_delimiter_no_split(self):
+        nodes = [TextNode("This is normal text", TextType.TEXT)]
+        result = split_nodes_delimiter(nodes, "**", TextType.BOLD)
+        expected = [TextNode("This is normal text", TextType.TEXT)]
+        self.assertEqual(result, expected)
+
+    def test_split_nodes_delimiter_invalid_syntax(self):
+        nodes = [TextNode("This is **bold text", TextType.TEXT)]
+        with self.assertRaises(ValueError):
+            split_nodes_delimiter(nodes, "**", TextType.BOLD)
+    
 if __name__ == "__main__":
     unittest.main()
 
--- src/textnode.py
+++ src/textnode.py
@@ -2,11 +2,11 @@ from enum import Enum
 #TextType = Enum("TextType", ["TEXT", "BOLD", "ITALIC", "CODE", "LINK", "IMAGE"])
 class TextType(Enum):
     TEXT = "TEXT"
-    BOLD = "BOLD"
-    ITALIC = "ITALIC"
-    CODE = "CODE"
-    LINK = "LINK"
-    IMAGE = "IMAGE"
+    BOLD = "BOLD"       # **bold**
+    ITALIC = "ITALIC"   # _italic_
+    CODE = "CODE"       # `code`
+    LINK = "LINK"       # [text](url)
+    IMAGE = "IMAGE"     # ![alt text](url)
 
 class TextNode:
     def __init__(self, text, texttype, url=None):
@@ -40,3 +40,29 @@ def text_node_to_html_node(text_node):
         return LeafNode("a", text_node.text, {"href": text_node.url})
     elif text_node.text_type == TextType.IMAGE:
         return LeafNode("img", None, {"src": text_node.url, "alt": text_node.text})
+
+def split_nodes_delimiter(old_nodes, delimiter, text_type):
+    new_nodes = []
+    split = []
+    for node in old_nodes:
+        if node.text_type != TextType.TEXT:
+            new_nodes.append(node)
+            continue
+        split = node.text.split(delimiter)
+        if len(split) <= 1:
+            new_nodes.append(node)
+            continue
+        if len(split) % 2 == 0:
+            # Even number of parts means odd number of delimiters
+            # e.g., "This is **bold** text" -> ["This is ", "bold", " text"] (3 parts, 2 delimiters)
+            raise ValueError("Invalid Markdown Syntax: Uneven number of delimiters in text:", node.text)
+        for i, part in enumerate(split):
+            if i % 2 == 0:
+                # Even index: normal text
+                if part:
+                    new_nodes.append(TextNode(part, TextType.TEXT))
+            else:
+                # Odd index: delimited text
+                new_nodes.append(TextNode(part, text_type))
+    # ToDo: Handle nested delimiters
+    return new_nodes
