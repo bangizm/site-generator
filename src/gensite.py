2af99e1063fe4289b69eb01f98b34ca6a466d14e
bangizm <bangizm@gmail.com>
2026-02-07 08:25:16 -0500

    Update public to static

--- src/gensite.py
+++ src/gensite.py
@@ -38,7 +38,7 @@ def generate_pages_recursive(dir_path_content, template_path, dest_dir_path, bas
 
 def generate_page(from_path, template_path, dest_path, basepath="/"):
 
-    print(f'....(base) Generating page from {from_path} to {dest_path} using template {template_path}')
+    print(f'...Generating page from {from_path} to {dest_path} using template {template_path}')
 
     with open(from_path, 'r') as f:    # read markdown file from from_path
         markdown_text = f.read()   
@@ -55,7 +55,6 @@ def generate_page(from_path, template_path, dest_path, basepath="/"):
     site_html = site_html.replace('href="/', f'href="{basepath}').replace('src="/', f'src="{basepath}')
     site_html = site_html.replace("href='/", f"href='{basepath}").replace("src='/", f"src='{basepath}")
 
-    print(f'__DEBUG__ {site_html=}')
 
     # write the resulting HTML to dest_path
     #----- print(f'Writing generated HTML to {dest_path}...{site_html=}')
@@ -74,7 +73,7 @@ def recursive_dircopy(src, dest):
         if os.path.isdir(src):
             if not os.path.exists(dest):
                 os.makedirs(dest)
-            items = os.listdir(src)   
+            items = os.listdir(src)
             for item in items:
                 s = os.path.join(src, item)
                 d = os.path.join(dest, item)
@@ -86,25 +85,25 @@ def recursive_dircopy(src, dest):
         print(f'Error copying static files: {e}')
 
 
-def copy_static_to_public():
+def copy_static_to_dest(dest_dir):
     try:
-        shutil.rmtree('./public')
-        print('Removed existing ./public directory...')
+        shutil.rmtree(dest_dir)
+        print(f'Removed existing {dest_dir} directory...')
     except Exception as e:
-        print(f'No existing ./public directory to remove: {e}. No problme. Continuing...')
+        print(f'No existing {dest_dir} directory to remove: {e}. No problme. Continuing...')
 
     try: 
         if not os.path.exists('./static'):
             print('Creating ./static directory...')
             os.mkdir('./static')    
-        if not os.path.exists('./public'):
-            print('Creating ./public directory...')
-            os.mkdir('./public')
+        if not os.path.exists(dest_dir):
+            print(f'Creating {dest_dir} directory...')
+            os.mkdir(dest_dir)
     except Exception as e:
         print(f'Error creating directories: {e}')
         return
        
     # Copy all files from ./static to ./public
     print('Copying contents of./static to ./public...')
-    recursive_dircopy('./static', './public')
+    recursive_dircopy('./static', dest_dir)
     print('Done!')
--- src/main.py
+++ src/main.py
@@ -4,15 +4,15 @@ import sys
 from blocknode import markdown_to_html_node
 from textnode import TextType, TextNode
 from htmlnode import HTMLNode, LeafNode, ParentNode
-from gensite import generate_page, generate_pages_recursive, copy_static_to_public, extract_title
+from gensite import copy_static_to_dest, generate_page, generate_pages_recursive, extract_title
 
 
 def main():
-    copy_static_to_public()
-
     basepath = sys.argv[1] if len(sys.argv) > 1 else "/"
+    dest_dir = './docs'
+    copy_static_to_dest(dest_dir)
     print(f" ... Generating sites using {basepath=}...")
-    generate_pages_recursive('./content', './template.html', './docs', basepath=basepath)
+    generate_pages_recursive('./content', './template.html', dest_dir, basepath=basepath)
     #generate_page('./content/index.md', './template.html', './public/index.html')
 
 
